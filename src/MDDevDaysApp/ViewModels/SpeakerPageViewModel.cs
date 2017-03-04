d91aecea8dc90458c650cbf1e45bb498709cf2d4
Andreas Richter <ar@anrichter.net>
2017-03-04 18:36:03 +0100

    Laden der Speaker erst, wenn der Tab aktiviert wird.

--- src/MDDevDaysApp/ViewModels/SpeakerPageViewModel.cs
+++ src/MDDevDaysApp/ViewModels/SpeakerPageViewModel.cs
@@ -1,20 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using MDDevDaysApp.DomainModel;
+using Prism;
 using Prism.Mvvm;
 
 namespace MDDevDaysApp.ViewModels
 {
-    public class SpeakerPageViewModel : BindableBase
+    public class SpeakerPageViewModel : BindableBase, IActiveAware
     {
         private readonly ISpeakers _speakers;
+        private bool _isActive;
 
         public SpeakerPageViewModel(ISpeakers speakers)
         {
             _speakers = speakers;
+            Speakers = new ObservableCollection<Speaker>();
             Title = "Sprecher";
+
+            PropertyChanged += OnPropertyChanged;
         }
 
         public string Title { get; }
-        public IEnumerable<Speaker> Speakers => _speakers.All();
+        public ObservableCollection<Speaker> Speakers { get; }
+
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { SetProperty(ref _isActive, value); }
+        }
+
+        public event EventHandler IsActiveChanged;
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == nameof(IsActive) && IsActive)
+                Activate();
+        }
+
+        private void Activate()
+        {
+            if (Speakers.Any())
+                return;
+
+            foreach (var speaker in _speakers.All())
+                Speakers.Add(speaker);
+        }
     }
 }
