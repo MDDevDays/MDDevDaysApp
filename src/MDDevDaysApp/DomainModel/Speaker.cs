9b39898498ca817fdb788edf991cc29dd06e0090
Andreas Richter <ar@anrichter.net>
2017-03-04 13:49:17 +0100

    Liste der Sprecher mit vollem Namen anzeigen

--- src/MDDevDaysApp/DomainModel/Speaker.cs
+++ src/MDDevDaysApp/DomainModel/Speaker.cs
@@ -7,5 +7,6 @@ namespace MDDevDaysApp.DomainModel
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
