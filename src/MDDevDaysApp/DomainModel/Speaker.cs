27d08f1070068b5b433da7155698f669fea617ed
Andreas Richter <ar@anrichter.net>
2017-04-02 16:06:38 +0200

    Sprecher um Lebenslauf als reiner Text erweitert, der auf der Sprecherseite angezeigt wird.

--- src/MDDevDaysApp/DomainModel/Speaker.cs
+++ src/MDDevDaysApp/DomainModel/Speaker.cs
@@ -9,6 +9,7 @@ namespace MDDevDaysApp.DomainModel
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ImageUri { get; set; }
+        public string Bio { get; set; }
 
         public string FullName => $"{AcademicTitle} {FirstName} {LastName}".Trim();
     }
