44e9816f5c6c3d7c4b2b29bdd7f4a04b278e9878
Andreas Richter <ar@anrichter.net>
2017-04-02 22:05:01 +0200

    Version auf 0.3 gesetzt (Cake-Task Version)

--- src/MDDevDaysApp/Properties/AssemblyInfo.cs
+++ src/MDDevDaysApp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Resources;
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -28,5 +28,5 @@ using Xamarin.Forms.Xaml;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
