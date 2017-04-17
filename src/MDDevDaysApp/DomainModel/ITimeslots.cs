ff61d3c2503ff47de20550d6afb2d0420c1e61eb
Andreas Richter <ar@anrichter.net>
2017-04-17 16:49:39 +0200

    Timeslots inklusive Daten implementiert

---
+++ src/MDDevDaysApp/DomainModel/ITimeslots.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDDevDaysApp.DomainModel
+{
+    public interface ITimeslots
+    {
+        Task<IEnumerable<Timeslot>> AllAsync();
+    }
+}
---
+++ src/MDDevDaysApp/DomainModel/Timeslot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MDDevDaysApp.DomainModel
+{
+    public class Timeslot
+    {
+        public Guid Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+        public string Title { get; set; }
+        public string Room { get; set; }
+    }
+}
