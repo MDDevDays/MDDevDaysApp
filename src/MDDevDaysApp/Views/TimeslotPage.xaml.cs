d379121e54dec5a72ce0f7789a307574fdfc733b
Andreas Richter <ar@anrichter.net>
2017-04-27 20:12:44 +0200

    Sprecher als Liste inklusive Foto auf einer Sessionseite anzeigen

--- src/MDDevDaysApp/Views/TimeslotPage.xaml.cs
+++ src/MDDevDaysApp/Views/TimeslotPage.xaml.cs
@@ -1,12 +1,34 @@
-﻿using Xamarin.Forms;
+﻿using System.ComponentModel;
+using MDDevDaysApp.ViewModels;
+using Xamarin.Forms;
 
 namespace MDDevDaysApp.Views
 {
     public partial class TimeslotPage : ContentPage
     {
+        private TimeslotPageViewModel _vm;
+
         public TimeslotPage()
         {
             InitializeComponent();
+
+            ViewModel.PropertyChanged += ViewModelOnPropertyChanged;
+        }
+
+        private TimeslotPageViewModel ViewModel => _vm ?? (_vm = BindingContext as TimeslotPageViewModel);
+
+        private void ViewModelOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == nameof(ViewModel.Timeslot) && ViewModel != null)
+                SpeakersListView.HeightRequest = ViewModel.Timeslot.Speakers.Count * SpeakersListView.RowHeight;
+        }
+
+        private void SpeakersListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
