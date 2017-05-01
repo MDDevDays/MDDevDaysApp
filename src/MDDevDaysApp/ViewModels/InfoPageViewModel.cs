89dd1fc6fc23f3f362ad182c8b2f8386df8ff451
Andreas Richter <ar@anrichter.net>
2017-05-01 17:21:56 +0200

    Doppelte Informationen (Name, Location, Datum) von der Startseite entfernt

--- src/MDDevDaysApp/ViewModels/InfoPageViewModel.cs
+++ src/MDDevDaysApp/ViewModels/InfoPageViewModel.cs
@@ -9,20 +9,15 @@ namespace MDDevDaysApp.ViewModels
         public InfoPageViewModel()
         {
             Title = "Infos";
+            
             // Angabe der Assembly ist notwendig, damit UWP im Release Mode das Bild auch anzeigt
             var assembly = typeof(InfoPageViewModel).GetTypeInfo().Assembly;
             BannerSource = ImageSource.FromResource("MDDevDaysApp.Images.Banner.png", assembly);
             LocationPlanSource = ImageSource.FromResource("MDDevDaysApp.Images.Lageplan.png", assembly);
-            ConferenceName = "Magdeburger Developer Days 2017";
-            ConferenceDate = "10.05. & 11.05.2017";
-            ConferenceLocation = "KONGRESS & KULTURWERK fichte in Magdeburg";
         }
 
         public string Title { get; }
         public ImageSource BannerSource { get; }
         public ImageSource LocationPlanSource { get; }
-        public string ConferenceName { get; }
-        public string ConferenceDate { get; }
-        public string ConferenceLocation { get; }
     }
 }
