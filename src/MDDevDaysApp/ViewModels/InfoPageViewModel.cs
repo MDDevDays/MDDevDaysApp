4e9dbf5ced3be1fb0bff414cbe8db3bc2c96d430
Andreas Richter <ar@anrichter.net>
2017-05-01 18:12:53 +0200

    Links zum Twitter-Stream und zum Twitter-Hashtag auf Startseite

--- src/MDDevDaysApp/ViewModels/InfoPageViewModel.cs
+++ src/MDDevDaysApp/ViewModels/InfoPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
 using Xamarin.Forms;
 
@@ -9,15 +12,43 @@ namespace MDDevDaysApp.ViewModels
         public InfoPageViewModel()
         {
             Title = "Infos";
-            
+
             // Angabe der Assembly ist notwendig, damit UWP im Release Mode das Bild auch anzeigt
             var assembly = typeof(InfoPageViewModel).GetTypeInfo().Assembly;
             BannerSource = ImageSource.FromResource("MDDevDaysApp.Images.Banner.png", assembly);
             LocationPlanSource = ImageSource.FromResource("MDDevDaysApp.Images.Lageplan.png", assembly);
+            OpenTwitterStream = new DelegateCommand(OpenTwitterStreamExecute);
+            OpenTwitterHashtag = new DelegateCommand(OpenTwitterHashtagExecute);
         }
 
         public string Title { get; }
         public ImageSource BannerSource { get; }
         public ImageSource LocationPlanSource { get; }
+        public ICommand OpenTwitterHashtag { get; set; }
+        public ICommand OpenTwitterStream { get; set; }
+
+        private void OpenTwitterStreamExecute()
+        {
+            try
+            {
+                Device.OpenUri(new Uri("twitter://user?id=4158533145"));
+            }
+            catch (Exception)
+            {
+                Device.OpenUri(new Uri("https://twitter.com/MiB_MD_DevDays"));
+            }
+        }
+
+        private void OpenTwitterHashtagExecute()
+        {
+            try
+            {
+                Device.OpenUri(new Uri("twitter://search?query=%23MDevDays"));
+            }
+            catch (Exception)
+            {
+                Device.OpenUri(new Uri("https://twitter.com/search?q=%23MDDevDays"));
+            }
+        }
     }
 }
